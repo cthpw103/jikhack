247f34dff342257847250770852c93ae90218531
cth103 <cthdev@email.com>
2019-08-21 23:05:55 +0100

    py rewrite

---
+++ index.py
@@ -0,0 +1,26 @@
+# mem man
+import client
+import threading
+import time
+
+client.run("csgo.exe");
+
+print("waiting for csgo...")
+client.findProcess()
+print("csgo process found")
+
+print("waiting for game modules...")
+client.findModules();
+print("modules found, client adress: "+client.clientModule.modBaseAddr+" engine adress: "+ client.engineModule.modBaseAddr)
+
+// load every cheat module
+client.loadCModules("modules");
+
+def hack(*args):
+    for m in client.modules:
+        m.run()
+
+    next=int(args[0])+1
+    threading.Timer(0.001, hack,[str(next)]).start()
+
+hack("1")
